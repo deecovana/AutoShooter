f62646d1f3acab192e5718e2ff50050ce385ec8d
DeeCovana <deecovana@gmail.com>
2026-06-28 23:36:10 +0300

    Add README.MD

---
+++ edit.cmd
@@ -0,0 +1,1 @@
+start C:\Godot4\Godot_v4.4.1-stable_win64\Godot_v4.4.1-stable_win64.exe Project\project.godot
---
+++ editInLatestBuild.cmd
@@ -0,0 +1,5 @@
+set folder=C:\Godot4\Godot_Latest\
+set dpcd=%~dp0
+cd %folder%
+pushd %folder%
+%folder%run.bat %dpcd%Project\project.godot
